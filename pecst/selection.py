1c4e6443f76e7a1aac50d5bb87bc1c87e0383d5d
gituser789 <62549000+gituser789@users.noreply.github.com>
2026-03-04 21:15:25 +0100

    fix selection flexible depending on technology

--- pecst/selection.py
+++ pecst/selection.py
@@ -17,15 +17,18 @@ def select_capacitors(c_requirements: CapacitorRequirements) -> tuple[list[str],
     c_foil_db_list: list[pd.DataFrame] = []
     c_ceramic_db_list: list[pd.DataFrame] = []
     c_electrolytic_db_list: list[pd.DataFrame] = []
+    technology_list: list[str] = []
     if CapacitorType.FilmCapacitor in c_requirements.capacitor_type_list:
         c_foil_name_list, c_foil_db_list = select_foil_capacitors(c_requirements)
+        technology_list.append("foil")
     if CapacitorType.CeramicCapacitor in c_requirements.capacitor_type_list:
         c_ceramic_name_list, c_ceramic_db_list = select_ceramic_capacitors(c_requirements)
+        technology_list.append("ceramic")
     if CapacitorType.ElectrolyticCapacitor in c_requirements.capacitor_type_list:
         c_electrolytic_name_list, c_electrolytic_db_list = select_electrolytic_capacitors(c_requirements)
+        technology_list.append("electrolytic")
 
     c_db_foil = pd.concat(c_foil_db_list)
+    c_db = [c_db_foil] + c_ceramic_db_list + c_electrolytic_db_list
 
-    c_db = c_ceramic_db_list + [c_db_foil] + c_electrolytic_db_list
-
-    return (["ceramic", "foil", "electrolytic"], c_db)
+    return technology_list, c_db
